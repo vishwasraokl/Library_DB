74204c28d1d97c6bfd4575554daf7ae1c7481b73
Vishwas <Vishwas>
2021-02-05 15:28:36 +0000

    table constraint

--- Library_DB/Library_DB/dbo/Tables/tblTransaction.sql
+++ Library_DB/Library_DB/dbo/Tables/tblTransaction.sql
@@ -1,11 +1,27 @@
-﻿CREATE TABLE [dbo].[tblTransaction] (
-	[TransactionId] INT NOT NULL IDENTITY (1,1)
-	,[BarrowerId] INT NOT NULL
-	,[BookId] INT NOT NULL
-	,[BarrowedDateTime] DATETIME NOT NULL
-	,[ReturnDate] DATETIME NULL
-	,[LastUpdated] DATETIME NULL
-	,CONSTRAINT [pK_tblTransaction] PRIMARY KEY CLUSTERED ([BookId] ASC)
-	,CONSTRAINT [FK_tblBarrower_tblTransaction] FOREIGN KEY ([BarrowerId]) REFERENCES [dbo].[tblBarrower]([Id])
-	,CONSTRAINT [FK_tblBook_tblTransaction] FOREIGN KEY ([TransactionId]) REFERENCES [dbo].[tblBarrower]([Id])
-	);
+﻿CREATE TABLE [dbo].[tblTransaction](
+	[TransactionId] [int] IDENTITY(1,1) NOT NULL,
+	[BarrowerId] [int] NOT NULL,
+	[BookId] [int] NOT NULL,
+	[BarrowedDateTime] [datetime] NOT NULL,
+	[ReturnDate] [datetime] NULL,
+	[LastUpdated] [datetime] NULL,
+ CONSTRAINT [pK_tblTransaction] PRIMARY KEY CLUSTERED 
+(
+	[TransactionId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tblTransaction]  WITH CHECK ADD  CONSTRAINT [FK_tblBarrower_tblTransaction] FOREIGN KEY([BarrowerId])
+REFERENCES [dbo].[tblBarrower] ([Id])
+GO
+
+ALTER TABLE [dbo].[tblTransaction] CHECK CONSTRAINT [FK_tblBarrower_tblTransaction]
+GO
+
+ALTER TABLE [dbo].[tblTransaction]  WITH CHECK ADD  CONSTRAINT [FK_tblBook_tblTransaction] FOREIGN KEY([BookId])
+REFERENCES [dbo].[tblBook] ([ID])
+GO
+
+ALTER TABLE [dbo].[tblTransaction] CHECK CONSTRAINT [FK_tblBook_tblTransaction]
+GO
